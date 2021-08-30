2f314941a40342b4b65b1d3d8daa39aa33c91849
alexan1152 <alexmail1152@gmail.com>
2021-08-30 23:49:24 +0300

    Home 01.01

--- index.php
+++ index.php
@@ -6,10 +6,35 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
     <?php wp_head() ?>
 </head>
 <body>
 <h1>Вот</h1>
+<ul id = "main-burger-menu">
+    <li><a href="#">Главное меню</a></li>
+    <li><a href="#">Каталог</a>
+        <ul id="main-burger-menu-catalog">
+            <li><a href="#">Люстры</a></li>
+            <li><a href="#">Бра и подсветки</a></li>
+            <li><a href="#">Споты</a></li>
+            <li><a href="#">Настольные лампы</a></li>
+            <li><a href="#">Торшеры</a></li>
+            <li><a href="#">Трековые системы</a></li>
+            <li><a href="#">Уличное освещение</a></li>
+            <li><a href="#">Офисное освещение</a></li>
+            <li><a href="#">Лампочки</a></li>
+            <li><a href="#">Светодиодная подсветка</a></li>
+            <li><a href="#">Предметы интерьера</a></li>
+            <li><a href="#">Электротовары</a></li>
+            <li><a href="#">Комплектующие</a></li>
+            <li><a href="#">Новогодние товары</a></li>
+        </ul>
+    </li>
+    <li><a href="#">Оплата</a></li>
+    <li><a href="#">Контакты</a></li>
+    <li><a href="#">Полная версия сайта</a></li>
+</ul>
 <?php wp_footer() ?>
 </body>
 </html>
